8268c02b83b56fe4a8771ddfe301a50717637ca2
kartikr <kartikr@adobe.com>
2024-03-04 09:32:19 +0530

    update for localstack

--- 02-overview/main.tf
+++ 02-overview/main.tf
@@ -7,8 +7,25 @@ terraform {
   }
 }
 
+# provider "aws" {
+#   region = "us-east-1"
+# }
+
 provider "aws" {
-  region = "us-east-1"
+  region                  = "us-east-1"
+  access_key              = "mock_access_key"
+  secret_key              = "mock_secret_key"
+  skip_credentials_validation = true
+  skip_metadata_api_check = true
+  skip_requesting_account_id = true  # for localstack
+  endpoints {
+    ec2       = "http://localhost:4566"
+    s3        = "http://localhost:4566"
+    dynamodb  = "http://localhost:4566"
+    lambda    = "http://localhost:4566"
+    kinesis   = "http://localhost:4566"
+    # Add other services endpoints as needed
+  }
 }
 
 resource "aws_instance" "example" {
--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # DevOps Directive Terraform Course
 
+> The tutorial has been updated to work with localstack desktop. Please download localstack desktop and run a localstack container with the image 'localstack/localstack'
+
 This is the companion repo to: [Complete Terraform Course - From BEGINNER to PRO! (Learn Infrastructure as Code)](https://www.youtube.com/watch?v=7xngnjfIlK4)
 
 [![thumbnail](https://user-images.githubusercontent.com/1320389/154354937-98533608-2f42-44c1-8110-87f7e3f45085.jpeg)](https://www.youtube.com/watch?v=7xngnjfIlK4)
