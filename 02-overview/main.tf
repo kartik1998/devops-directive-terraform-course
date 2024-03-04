2992422584c825a0e55e8eae9608396ea5f2277c
kartikr <kartikr@adobe.com>
2024-03-04 14:02:47 +0530

    format tf file

--- 02-overview/main.tf
+++ 02-overview/main.tf
@@ -12,18 +12,18 @@ terraform {
 # }
 
 provider "aws" {
-  region                  = "us-east-1"
-  access_key              = "mock_access_key"
-  secret_key              = "mock_secret_key"
+  region                      = "us-east-1"
+  access_key                  = "mock_access_key"
+  secret_key                  = "mock_secret_key"
   skip_credentials_validation = true
-  skip_metadata_api_check = true
-  skip_requesting_account_id = true  # for localstack
+  skip_metadata_api_check     = true
+  skip_requesting_account_id  = true # for localstack
   endpoints {
-    ec2       = "http://localhost:4566"
-    s3        = "http://localhost:4566"
-    dynamodb  = "http://localhost:4566"
-    lambda    = "http://localhost:4566"
-    kinesis   = "http://localhost:4566"
+    ec2      = "http://localhost:4566"
+    s3       = "http://localhost:4566"
+    dynamodb = "http://localhost:4566"
+    lambda   = "http://localhost:4566"
+    kinesis  = "http://localhost:4566"
     # Add other services endpoints as needed
   }
 }
