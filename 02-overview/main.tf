c0f8056e7b1beef74b3f876fbfcb20afe47ed18f
kartikr <kartikr@adobe.com>
2024-03-05 09:45:23 +0530

    add s3_force_path_style for localstack s3 domain

--- 02-overview/main.tf
+++ 02-overview/main.tf
@@ -15,6 +15,7 @@ provider "aws" {
   region                      = "us-east-1"
   access_key                  = "mock_access_key"
   secret_key                  = "mock_secret_key"
+  s3_force_path_style         = true
   skip_credentials_validation = true
   skip_metadata_api_check     = true
   skip_requesting_account_id  = true # for localstack
@@ -28,7 +29,34 @@ provider "aws" {
   }
 }
 
-resource "aws_instance" "example" {
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = "terraform.state.bucket"
+  acl    = "private"
+  # force_destroy = true
+  # versioning {
+  #   enabled = true
+  # }
+
+  # server_side_encryption_configuration {
+  #   rule {
+  #     apply_server_side_encryption_by_default {
+  #       sse_algorithm = "AES256"
+  #     }
+  #   }
+  # }
+}
+
+resource "aws_dynamodb_table" "terraform_locks" {
+  name         = "terraform-state-locking"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
+
+resource "aws_instance" "sample_ec2_instance" {
   ami           = "ami-011899242bb902164" # Ubuntu 20.04 LTS // us-east-1
   instance_type = "t2.micro"
 }
